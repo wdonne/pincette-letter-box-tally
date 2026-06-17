524e0e73d29bc91eab114f0213cbfcbc1c894a84
Werner Donné <werner.donne@pincette.biz>
2026-06-17 19:43:14 +0200

    Update dependencies

--- src/main/java/net/pincette/letterbox/tally/Common.java
+++ src/main/java/net/pincette/letterbox/tally/Common.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 class Common {
   static final Logger LOGGER = getLogger("net.pincette.letterbox.tally");
   static final String LETTER_BOX_TALLY = "letter-box-tally";
-  static final String VERSION = "1.1.7";
+  static final String VERSION = "1.1.8";
 
   private Common() {}
 }
